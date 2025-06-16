ac3ea8c2137a97e6836c8017ea91add6fe80061b
hnin-hnin-wai <50986766+hnin-hnin-wai@users.noreply.github.com>
2025-06-16 18:00:34 -0500

    add refresh token

--- backend/server.ts
+++ backend/server.ts
@@ -8,6 +8,8 @@ import path from "path";
 import userRoutes from "./users/user.route";
 import questionRoutes from "./questions/question.route";
 import { checkToken } from "./users/user.middleware";
+import cookieParser from "cookie-parser";
+
 import { routerNotFoundHandler, errorHandler } from "./utils/common";
 
 dotenv.config();
@@ -15,18 +17,26 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGODB_URI!;
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:4200";
 
 mongoose.connect(MONGO_URI);
 
 app.use(morgan("dev"));
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.use(json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/users", userRoutes);
 app.use("/questions", checkToken, questionRoutes); // 🔐 Protected route
 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 /* app.use(routerNotFoundHandler);
 app.use(errorHandler); */
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
